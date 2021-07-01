99402931a73c67292da9fa10f061266edc3b6d14
Timothy Ling <timothyling1994@gmail.com>
2021-07-01 12:07:31 -0700

    Implemented Category Create and Delete across controller and views

--- inventoryapp/controllers/categoryController.js
+++ inventoryapp/controllers/categoryController.js
@@ -1,5 +1,6 @@
 const { body,validationResult } = require('express-validator');
 var Category = require('../models/category');
+var Item = require('../models/item');
 var async = require('async');
 
 // Display list of all categorys.
@@ -20,28 +21,102 @@ exports.category_list = function(req, res) {
 
 // Display detail page for a specific category.
 exports.category_detail = function(req, res, next) {
-    
+    Category.findById(req.params.id).exec(function(err,category){
+        if(err){
+            return next(err);
+        }
+
+        res.render('category_detail',{title:'Category: '+category.name, category:category});
+    });
 };
 
 // Display category create form on GET.
 exports.category_create_get = function(req, res) {
-
+    res.render('category_form', { title: 'Create Category'});
 };
 
 // Handle category create on POST.
 exports.category_create_post = [
-    
+    body('category_name').trim().isLength({ min: 1 }).escape().withMessage('Category name must be specified.')
+        .isAlphanumeric().withMessage('First name has non-alphanumeric characters.'),
+    body('category_description').escape(),
+
+    (req, res, next) => {
+
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            console.log("errors");
+            // There are errors. Render form again with sanitized values/errors messages.
+            res.render('category_form', { title: 'Create Category', category: req.body, errors: errors.array() });
+            return;
+        }
+
+        else
+        {
+            var category = new Category(
+            { 
+                name: req.body.category_name,
+                description: req.body.category_description,
+            });
+
+            category.save(function (err) {
+                if (err) { return next(err); }
+                // Successful - redirect to new author record.
+                res.redirect(category.url);
+            });
+        }
+
+    }
 ];
 
 // Display category delete form on GET.
 exports.category_delete_get = function(req, res,next) {
-    
+    async.parallel({
+        category: function(callback) {
+            Category.findById(req.params.id).exec(callback)
+        },
+        category_items: function(callback) {
+          Item.find({ 'category': req.params.id }).exec(callback)
+        },
+    }, function(err, results) {
+        if (err) { return next(err); }
+        if (results.category==null) { // No results.
+            res.redirect('/catalog/');
+        }
+        // Successful, so render.
+        res.render('category_delete', { title: 'Delete Category', category: results.category, category_items: results.category_items } );
+    });
 
 };
 
 // Handle category delete on POST.
-exports.category_delete_post = function(req, res) {
-    
+exports.category_delete_post = function(req, res,next) {
+    async.parallel({
+        category: function(callback) {
+            Category.findById(req.params.id).exec(callback)
+        },
+        category_items: function(callback) {
+          Item.find({ 'category': req.params.id }).exec(callback)
+        },
+    }, function(err, results) {
+        if (err) { return next(err); }
+        // Success
+        if (results.category_items.length > 0) {
+            // Genre has books. Render in same way as for GET route.
+            res.render('category_delete', { title: 'Delete Category', category: results.category, category_items: results.category_items } );
+            return;
+        }
+        else {
+            // Author has no books. Delete object and redirect to the list of authors.
+            Category.findByIdAndRemove(req.body.categoryid, function deleteGenre(err) {
+                if (err) { return next(err); }
+                // Success - go to author list
+                res.redirect('/catalog/')
+            })
+        }
+    });
+
 };
 
 // Display category update form on GET.
--- inventoryapp/routes/catalog.js
+++ inventoryapp/routes/catalog.js
@@ -9,16 +9,23 @@ var item_controller = require('../controllers/itemController');
 router.get('/', category_controller.category_list);
 // GET request for creating a Category. NOTE This must come before routes that display Category (uses id).
 router.get('/category/create', category_controller.category_create_get);
+// POST request for creating Category.
+router.post('/category/create', category_controller.category_create_post);
+
+
+// GET request to delete Category.
+router.get('/category/:id/delete', category_controller.category_delete_get);
+// POST request to delete Category.
+router.post('/category/:id/delete', category_controller.category_delete_post);
+
+// GET request for one Category.
+router.get('/category/:id', category_controller.category_detail);
 
 /*
 // POST request for creating Category.
 router.post('/category/create', category_controller.category_create_post);
 
-// GET request to delete Category.
-router.get('/category/:id/delete', category_controller.category_delete_get);
 
-// POST request to delete Category.
-router.post('/category/:id/delete', category_controller.category_delete_post);
 
 // GET request to update Category.
 router.get('/category/:id/update', category_controller.category_update_get);
@@ -26,8 +33,6 @@ router.get('/category/:id/update', category_controller.category_update_get);
 // POST request to update Category.
 router.post('/category/:id/update', category_controller.category_update_post);
 
-// GET request for one Category.
-router.get('/category/:id', category_controller.category_detail);
 
 // GET request for list of all Category items.
 router.get('/categories', category_controller.category_list);*/
